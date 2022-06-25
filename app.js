fc2873c570a9fcbcb78b26b59b40ce8a43dd1785
jimish1806 <jimish957@gmail.com>
2022-06-26 01:11:10 +0530

    set random background functionality

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 console.log("Jay Shree Ram");
+
+
+// -----------------> Functions <------------------//
+function randomNumberGenerator()
+{
+    return Math.floor(Math.random()*(6)+(1));
+}
+
+let container = document.getElementById("container");
+// console.log(container);
+
+container.style.background = `url(./images/bg${randomNumberGenerator()}.jpg)`;
+container.style.backgroundPosition = `center`;
+container.style.backgroundSize = `cover`;
