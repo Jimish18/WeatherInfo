9a06f472809114c95ebae62241b92ade2e916c33
jimish1806 <jimish957@gmail.com>
2022-07-09 12:03:40 +0530

    added some bg imgs

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ console.log("Jay Shree Ram");
 
 
 // -----------------> Functions <------------------//
-function randomNumberGenerator()
-{
-    return Math.floor(Math.random()*(6)+(1));
-}
+// function randomNumberGenerator()
+// {
+//     return Math.floor(Math.random()*(6)+(1));
+// }
 
-let container = document.getElementById("container");
-// console.log(container);
+// let container = document.getElementById("container");
+// // console.log(container);
 
-container.style.background = `url(./images/bg${randomNumberGenerator()}.jpg)`;
-container.style.backgroundPosition = `center`;
-container.style.backgroundSize = `cover`;
+// container.style.background = `url(./images/bg${randomNumberGenerator()}.jpg)`;
+// container.style.backgroundPosition = `center`;
+// container.style.backgroundSize = `cover`;
